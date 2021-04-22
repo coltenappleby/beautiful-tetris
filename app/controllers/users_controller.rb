74e576b58958efc805a8455bea4a161195655d54
coltenappleby <colten.appleby@gmail.com>
2021-04-22 14:34:50 -0400

    user index api working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+
+
+    def index
+        users = User.all
+        render json: users
+        # render json: users.to_json(:include => {:id, :username}, :except => [:created_at, :updated_at])
+    end
+
+
 end
+
+# class UserSerializer
+
+#     def initialize(user_object)
+#         @user = user_object
+#     end
+
+#     def to_serialized_json
+#         @users.to_json(:include => {
+#         :username,
+#         :id },
+#         :except => [:updated_at, :created_at])
+#     end
+
+# end
